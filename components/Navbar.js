4ccd967c3c6fe09c9c8801f43f9ce12bc618f01d
nischaldutt <nischaldutt01@gmail.com>
2022-08-10 16:46:49 +0530

    update: moving navbar to _app

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,12 @@
-function Navbar({ headers }) {
+export default function Navbar({ headers }) {
   return (
-    <div className="border py-4 flex justify-center sm:justify-between px-8 bg-secondary">
+    <div className="border-0 py-4 flex justify-center sm:justify-between px-8 bg-secondary">
       <div>svg icon</div>
       <ul className="flex">
         {headers.map((head) => {
           return (
             <div
-              className="px-4 hidden sm:block text-white hover:text-gray-200 cursor-pointer"
+              className="px-4 hidden sm:block text-white hover:text-gray-300 cursor-pointer"
               key={head.label}
             >
               {head.label}
@@ -17,5 +17,3 @@ function Navbar({ headers }) {
     </div>
   );
 }
-
-export default Navbar;
