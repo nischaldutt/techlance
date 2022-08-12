fef4779b5197f8e11e86520fd8aee72810ce047b
nischaldutt <nischaldutt01@gmail.com>
2022-08-12 11:59:35 +0530

    styles: fixing styles in login form and removing hero image

--- components/SigninForm.js
+++ components/SigninForm.js
@@ -4,12 +4,14 @@ import SocialButton from "./SocialButton";
 
 export default function SigninForm() {
   return (
-    <div className="border border-red-900  px-12 py-8">
-      <div className="text-2xl font-bold">
+    <div className="px-8 sm:px-12 py-8">
+      <div className="text-2xl font-bold pb-4">
         <BsBullseye />
       </div>
-      <div className="text-4xl font-bold">Sign in to your account</div>
-      <div className="text-gray-500">Sign in with</div>
+      <div className="text-3xl sm:text-4xl font-bold py-2">
+        Sign in to your account
+      </div>
+      <div className="text-gray-500 py-2">Sign in with</div>
 
       <div className="grid grid-cols-3 gap-4">
         <SocialButton SocialIcon={<BsGoogle />} />
@@ -17,9 +19,13 @@ export default function SigninForm() {
         <SocialButton SocialIcon={<BsFacebook />} />
       </div>
 
-      <div className="text-gray-500">Or continue with</div>
+      <div className="grid grid-cols-3 gap-1 items-center py-4">
+        <hr />
+        <div className="text-gray-500 text-xs sm:text-sm">Or continue with</div>
+        <hr />
+      </div>
 
-      <div className="col-span-6 sm:col-span-4">
+      <div className="pb-2">
         <label
           htmlFor="email-address"
           className="block text-sm font-medium text-gray-700"
@@ -33,11 +39,11 @@ export default function SigninForm() {
           autoComplete="email"
           placeholder="john_doe@gmail.com"
           className="mt-1 p-2 block w-full shadow-sm sm:text-sm border
-          border-gray-300 focus:border-gray-700 rounded-md text-gray-700"
+          border-gray-300 rounded-md text-gray-700"
         />
       </div>
 
-      <div className="col-span-6 sm:col-span-4">
+      <div className="py-2">
         <label
           htmlFor="password"
           className="block text-sm font-medium text-gray-700"
@@ -54,33 +60,33 @@ export default function SigninForm() {
         />
       </div>
 
-      <div className="grid grid-cols-2">
-        <div className="flex items-start">
-          <div className="flex items-center h-5">
-            <input
-              id="remember"
-              name="remember"
-              type="checkbox"
-              className="focus:ring-primary h-4 w-4 text-primary border-gray-300 rounded"
-            />
-          </div>
-          <div className="ml-3 text-sm">
+      <div className="grid grid-cols-2 py-4">
+        <div className="flex items-center">
+          <input
+            id="remember"
+            name="remember"
+            type="checkbox"
+            className="focus:ring-primary h-4 w-4 text-primary border-gray-300 rounded"
+          />
+          <div className="ml-2 text-sm">
             <label htmlFor="remember" className="font-medium text-gray-700">
               Remember me
             </label>
           </div>
         </div>
 
-        <div className="text-primary">Forgot your password?</div>
+        <div className="text-primary text-xs sm:text-sm flex justify-end">
+          Forgot your password?
+        </div>
       </div>
 
-      <div className="w-full">
+      <div className="w-full py-2">
         <button
           type="submit"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold 
           py-2 px-4 rounded w-full"
         >
-          Button
+          Sign in
         </button>
       </div>
     </div>
--- pages/signin.js
+++ pages/signin.js
@@ -3,13 +3,6 @@ import Image from "next/image";
 
 import SigninForm from "../components/SigninForm";
 
-const imageSrc =
-  "https://images.unsplash.com/photo-1553272725-086100aecf5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80";
-
-const myLoader = ({ src }) => {
-  return src;
-};
-
 export default function Signin() {
   return (
     <>
@@ -20,17 +13,8 @@ export default function Signin() {
       </Head>
 
       <div className="border border-black flex justify-center">
-        <div className="border border-black">
+        <div className="border">
           <SigninForm />
-        </div>
-        <div className="border border-black">
-          <Image
-            loader={myLoader}
-            src={imageSrc}
-            width={700}
-            height={900}
-            alt="ds"
-          />
         </div>
       </div>
     </>
