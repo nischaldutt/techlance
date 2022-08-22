25231370bd03dff6184d9f02ee665c8910be1874
Nischal Dutt <nischaldutt01@gmail.com>
2022-08-23 00:53:27 +0530

    feat/login (#1)
    * setup: tailwindcss
    
    * setup: configuring theme
    
    * update: installing react-icons
    
    * update: moving navbar to _app
    
    * feat: setting up sign in page
    
    * styles: fixing styles in login form and removing hero image
    
    * update: whitelisting domains
    
    * feat: initial navbar setup
    
    * update: adding hero video
    
    * feat: setting up application fonts
    
    * feat: adding typewrite effect to the hero section
    
    * feat: adding search bar and location bar and making them responsive

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import NavBar from "../components/Navbar";
+
+const headers = [
+  { label: "Explore Services" },
+  { label: "Become a Seller" },
+  { label: "Login" },
+  { label: "Join" },
+];
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavBar headers={headers} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  mode: "jit",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0B1D27",
+        secondary: "#983333",
+      },
+    },
+  },
+  plugins: [],
+};
