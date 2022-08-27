5f798c70dfda44838254e02a26d07eb27869a62d
nischaldutt <nischaldutt01@gmail.com>
2022-08-28 01:39:09 +0530

    update: adding assets

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,11 @@
 import ServicesSvg from "./svg/Services.svg";
+import Bookings from "./svg/Bookings.svg";
+import Reminders from "./svg/Reminders.svg";
 
 const assets = {
   ServicesSvg,
+  Bookings,
+  Reminders,
 };
 
 export default assets;
