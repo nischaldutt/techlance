d401601c74d6d1282830693105a0d355fcc318e7
nischaldutt <nischaldutt01@gmail.com>
2022-08-28 19:30:03 +0530

    feat: adding login assets

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,4 @@
 export { default as Services } from "./svg/Services.svg";
-export { default as Bookings } from "./svg/Services.svg";
-export { default as Reminders } from "./svg/Services.svg";
+export { default as Bookings } from "./svg/Bookings.svg";
+export { default as Reminders } from "./svg/Reminders.svg";
+export { default as Signin } from "./svg/Signin.svg";
