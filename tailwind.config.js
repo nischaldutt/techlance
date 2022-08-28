b2f663b9cca7af6adba6eb64bdaafde020fd3e44
nischaldutt <nischaldutt01@gmail.com>
2022-08-28 19:33:22 +0530

    theme: adding keyframe in theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,12 @@ module.exports = {
         primary: "#0B1D27",
         secondary: "#983333",
       },
+      keyframes: {
+        float: {
+          "0%, 100%": { transform: "translatey(0px)" },
+          "50%": { transform: "translatey(-20px)" },
+        },
+      },
     },
     screens: {
       xs: "475px",
