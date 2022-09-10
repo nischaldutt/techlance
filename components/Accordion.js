df0ff808f30ea0677f4af05b74140b674d23ee60
nischaldutt <nischaldutt01@gmail.com>
2022-09-10 20:17:12 +0530

    fix: fixing according

--- components/Accordion.js
+++ components/Accordion.js
@@ -1,143 +1,40 @@
+import React from "react";
 import Link from "next/link";
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from "react-icons/md";
 
-const Accordion = () => {
+import { ServiceListItem } from "../components";
+
+const Accordion = ({ headLabel, initialExpand }) => {
+  const [isVisible, setIsVisible] = React.useState(initialExpand);
+
+  const handleToggle = () => {
+    setIsVisible(() => !isVisible);
+  };
+
   return (
-    <div className="accordion" id="accordionExample">
-      <div className="accordion-item bg-white border border-gray-200">
-        <h2 className="accordion-header mb-0" id="headingOne">
+    <div className="accordion" id="accordionExample5">
+      <div className="accordion-item bg-white">
+        <h2 className="">
           <button
-            className="
-        accordion-button
-        relative
-        flex
-        items-center
-        w-full
-        py-4
-        px-5
-        text-base text-gray-800 text-left
-        bg-white
-        border-0
-        rounded-none
-        transition
-        focus:outline-none
-      "
+            className="relative flex items-center w-full py-3 
+              border border-b-gray-200 border-t-gray-200 bg-white 
+              justify-between"
             type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapseOne"
-            aria-expanded="true"
-            aria-controls="collapseOne"
+            onClick={handleToggle}
           >
-            Accordion Item #1
+            <div className="text-gray-800 text-sm">{headLabel}</div>
+            {isVisible ? (
+              <MdKeyboardArrowUp size="20" />
+            ) : (
+              <MdKeyboardArrowDown size="20" />
+            )}
           </button>
         </h2>
-        <div
-          id="collapseOne"
-          className="accordion-collapse collapse show"
-          aria-labelledby="headingOne"
-          data-bs-parent="#accordionExample"
-        >
-          <div className="accordion-body py-4 px-5">
-            <strong>This is the first accordion body.</strong> It is shown by
-            default, until the collapse plugin adds the appropriate classes that
-            we use to style each element. These classes control the overall
-            appearance, as well as the showing and hiding via CSS transitions.
-            You can modify any of this with custom CSS or overriding our default
-            variables. also worth noting that just about any HTML can go within
-            the <code>.accordion-body</code>, though the transition does limit
-            overflow.
-          </div>
-        </div>
-      </div>
-      <div className="accordion-item bg-white border border-gray-200">
-        <h2 className="accordion-header mb-0" id="headingTwo">
-          <button
-            className="
-        accordion-button
-        collapsed
-        relative
-        flex
-        items-center
-        w-full
-        py-4
-        px-5
-        text-base text-gray-800 text-left
-        bg-white
-        border-0
-        rounded-none
-        transition
-        focus:outline-none
-      "
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapseTwo"
-            aria-expanded="false"
-            aria-controls="collapseTwo"
-          >
-            Accordion Item #2
-          </button>
-        </h2>
-        <div
-          id="collapseTwo"
-          className="accordion-collapse collapse"
-          aria-labelledby="headingTwo"
-          data-bs-parent="#accordionExample"
-        >
-          <div className="accordion-body py-4 px-5">
-            <strong>This is the second accordion body.</strong> It is hidden by
-            default, until the collapse plugin adds the appropriate classes that
-            we use to style each element. These classes control the overall
-            appearance, as well as the showing and hiding via CSS transitions.
-            You can modify any of this with custom CSS or overriding our default
-            variables. also worth noting that just about any HTML can go within
-            the <code>.accordion-body</code>, though the transition does limit
-            overflow.
-          </div>
-        </div>
-      </div>
-      <div className="accordion-item bg-white border border-gray-200">
-        <h2 className="accordion-header mb-0" id="headingThree">
-          <button
-            className="
-        accordion-button
-        collapsed
-        relative
-        flex
-        items-center
-        w-full
-        py-4
-        px-5
-        text-base text-gray-800 text-left
-        bg-white
-        border-0
-        rounded-none
-        transition
-        focus:outline-none
-      "
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapseThree"
-            aria-expanded="false"
-            aria-controls="collapseThree"
-          >
-            Accordion Item #3
-          </button>
-        </h2>
-        <div
-          id="collapseThree"
-          className="accordion-collapse collapse"
-          aria-labelledby="headingThree"
-          data-bs-parent="#accordionExample"
-        >
-          <div className="accordion-body py-4 px-5">
-            <strong>This is the third accordion body.</strong> It is hidden by
-            default, until the collapse plugin adds the appropriate classes that
-            we use to style each element. These classes control the overall
-            appearance, as well as the showing and hiding via CSS transitions.
-            You can modify any of this with custom CSS or overriding our default
-            variables. also worth noting that just about any HTML can go within
-            the <code>.accordion-body</code>, though the transition does limit
-            overflow.
-          </div>
+        <div className={isVisible ? "" : "hidden"}>
+          <ServiceListItem />
+          <ServiceListItem />
+          <ServiceListItem />
+          <ServiceListItem />
         </div>
       </div>
     </div>
