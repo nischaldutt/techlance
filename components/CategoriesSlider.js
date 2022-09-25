6b9e8ccf2447bb11bf924df23ee468ab192a069b
nischaldutt <nischaldutt01@gmail.com>
2022-09-25 14:49:12 +0530

    update: fixing category card

--- components/CategoriesSlider.js
+++ components/CategoriesSlider.js
@@ -3,18 +3,20 @@ import Image from "next/image";
 
 const Category = () => {
   return (
-    <div className="bg-white rounded-lg shadow-lg drop-shadow-lg grid place-items-center shrink-0 w-[170px] h-[207px] px-4 pt-8 pb-4 transition-[margin] duration-300 hover:mt-[-8px]">
-      <div className="relative w-[97px] h-[97px]">
-        <Image
-          src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
-          className="rounded-full w-32"
-          alt="Avatar"
-          layout="fill"
-          objectFit="contain"
-        />
+    <Link href="/">
+      <div className="bg-white cursor-pointer rounded-lg shadow-lg drop-shadow-lg grid place-items-center shrink-0 w-[170px] h-[207px] px-4 pt-8 pb-4 transition-[margin] duration-300 hover:mt-[-8px]">
+        <div className="relative w-[97px] h-[97px]">
+          <Image
+            src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+            className="rounded-full w-32"
+            alt="Avatar"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <div className="font-bold text-gray-700">Hair Salon</div>
       </div>
-      <div>Hair Saloon</div>
-    </div>
+    </Link>
   );
 };
 
