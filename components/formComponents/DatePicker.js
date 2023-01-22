f30e1179860359b90f90636f23d3761a384cb896
nischaldutt <nischaldutt01@gmail.com>
2023-01-22 19:57:03 +0530

    feat: creating date picker

--- components/formComponents/DatePicker.js
+++ components/formComponents/DatePicker.js
@@ -1,38 +1,11 @@
-import React, { useState } from "react";
-import DatePicker from "react-datepicker";
+import { DatePicker } from "antd";
 
-import "react-datepicker/dist/react-datepicker.css";
+const onChange = (date, dateString) => {
+  console.log(date, dateString);
+};
 
-// CSS Modules, react-datepicker-cssmodules.css
-// import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+const BasicDatePicker = () => {
+  return <DatePicker status="error" onChange={onChange} />;
+};
 
-export default function Example() {
-  const [startDate, setStartDate] = useState(new Date());
-
-  const handleCalendarClose = () => console.log("Calendar closed");
-  const handleCalendarOpen = () => console.log("Calendar opened");
-
-  const ExampleCustomInput = React.forwardRef(({ value, onClick }, ref) => (
-    <button className="example-custom-input" onClick={onClick} ref={ref}>
-      {value}
-    </button>
-  ));
-
-  let handleColor = (time) => {
-    return time.getHours() > 12 ? "text-success" : "text-error";
-  };
-
-  return (
-    <DatePicker
-      selected={startDate}
-      onChange={(date) => setStartDate(date)}
-      onCalendarClose={handleCalendarClose}
-      onCalendarOpen={handleCalendarOpen}
-      isClearable
-      customInput={<ExampleCustomInput />}
-      showTimeSelect
-    >
-      <div style={{ color: "red" }}>Dont forget to check the weather!</div>
-    </DatePicker>
-  );
-}
+export default BasicDatePicker;
