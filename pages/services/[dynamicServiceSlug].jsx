22144eeb106c168b4742229fdb3586396fed3cf5
nischaldutt <nischaldutt01@gmail.com>
2023-02-19 22:00:31 +0530

    update: fixing project layout

--- pages/services/[dynamicServiceSlug].jsx
+++ pages/services/[dynamicServiceSlug].jsx
@@ -1,9 +1,10 @@
+import React from "react";
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
 
-import { CardSlider, ServiceSearchCard, Footer } from "../../components";
+import { CardSlider, ServiceSearchCard, Footer } from "@/components";
 
 export default function AvailableServices() {
   const router = useRouter();
--- pages/services/[dynamicServiceSlug]/[serviceId].jsx
+++ pages/services/[dynamicServiceSlug]/[serviceId].jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
@@ -19,7 +20,7 @@ import {
   RatingStats,
   StaticMap,
   Footer,
-} from "../../../components";
+} from "@/components";
 
 const galleryImages = [
   {
