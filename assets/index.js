45d363b7b2af88992b32c288c2595d70de640bcc
nischaldutt <nischaldutt01@gmail.com>
2023-02-19 21:47:37 +0530

    update: saving assets

--- assets/index.js
+++ assets/index.js
@@ -4,3 +4,5 @@ export { default as Reminders } from "./svg/Reminders.svg";
 export { default as Signin } from "./svg/Signin.svg";
 export { default as Tick } from "./svg/Tick.svg";
 export { default as Upload } from "./svg/Upload.svg";
+export { default as AboutImageOne } from "./svg/about-image-1.svg";
+export { default as AboutImageTwo } from "./svg/about-image-2.svg";
