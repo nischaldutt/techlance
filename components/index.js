1be485ad8abf0721cea7b2ad3e9b3e53abeb99f8
nischaldutt <nischaldutt01@gmail.com>
2023-02-19 22:01:27 +0530

    update: exporting auto imported components

--- components/index.js
+++ components/index.js
@@ -1,19 +1,38 @@
-export { default as HomeHeroComponent } from "./HomeHeroComponent";
-export { default as Footer } from "./Footer";
-export { default as Navbar } from "./Navbar";
-export { default as SigninForm } from "./SigninForm";
-export { default as SocialButton } from "./SocialButton";
-export { default as SignupForm } from "./SignupForm";
-export { default as ServiceCard } from "./ServiceCard";
-export { default as CardSlider } from "./CardSlider";
-export { default as Carousal } from "./Carousal";
-export { default as Accordion } from "./Accordion";
-export { default as Collapse } from "./Collapse";
-export { default as ServiceListItem } from "./ServiceListItem";
-export { default as Gallery } from "./Gallery";
-export { default as StaticMap } from "./StaticMap";
-export { default as RatingStats } from "./RatingStats";
-export { default as CategoriesSlider } from "./CategoriesSlider";
-export { default as ServiceSearchCard } from "./ServiceSearchCard";
-export { default as ServiceNavHeader } from "./ServiceNavHeader";
-export { default as JobRateCard } from "./JobRateCard";
+// common
+export { default as SectionTitle } from "@/components/common/SectionTitle";
+export { default as Contact } from "@/components/common/Contact";
+export { default as About } from "@/components/common/About";
+
+// client
+export { default as ClientHero } from "@/components/client/Hero";
+export { default as ClientAboutSectionOne } from "@/components/client/About/AboutSectionOne";
+export { default as ClientAboutSectionTwo } from "@/components/client/About/AboutSectionTwo";
+export { default as ClientAboutSectionThree } from "@/components/client/About/AboutSectionThree";
+export { default as ClientNavbar } from "@/components/client/NavBar";
+export { default as ClientRecommendations } from "@/components/client/Recommendations";
+export { default as ClientServiceCategories } from "@/components/client/Categories";
+
+export { default as Footer } from "@/components/Footer";
+export { default as SigninForm } from "@/components/SigninForm";
+export { default as SocialButton } from "@/components/SocialButton";
+export { default as SignupForm } from "@/components/SignupForm";
+export { default as ServiceCard } from "@/components/ServiceCard";
+export { default as CardSlider } from "@/components/CardSlider";
+export { default as Carousal } from "@/components/Carousal";
+export { default as Accordion } from "@/components/Accordion";
+export { default as Collapse } from "@/components/Collapse";
+export { default as ServiceListItem } from "@/components/ServiceListItem";
+export { default as Gallery } from "@/components/Gallery";
+export { default as StaticMap } from "@/components/StaticMap";
+export { default as RatingStats } from "@/components/RatingStats";
+export { default as CategoriesSlider } from "@/components/CategoriesSlider";
+export { default as ServiceSearchCard } from "@/components/ServiceSearchCard";
+export { default as ServiceNavHeader } from "@/components/ServiceNavHeader";
+export { default as JobRateCard } from "@/components/JobRateCard";
+
+// business
+export { default as BusinessHero } from "@/components/business/Hero";
+export { default as BusinessFeatures } from "@/components/business/Features";
+export { default as BusinessAboutSectionOne } from "@/components/business/About/AboutSectionOne";
+export { default as BusinessAboutSectionTwo } from "@/components/business/About/AboutSectionTwo";
+export { default as BusinessPricing } from "@/components/business/Pricing";
