9329c140c72e07b6af73703467799d6810c2dc63
nischal <nischaldutt01@gmail.com>
2023-05-07 23:25:35 +0530

    feat: using react-hook-form to create controlled form components

--- pages/auth/signup/SignUpForm.jsx
+++ pages/auth/signup/SignUpForm.jsx
@@ -1,89 +1,179 @@
-import Link from "next/link";
-import { BsGoogle, BsTwitter, BsFacebook, BsBullseye } from "react-icons/bs";
+import { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+import { Button, Form, Input, Select } from "antd";
 
-import { ClientSocialButton } from "@/components";
+const { Option } = Select;
 
 export default function SignUpForm() {
+  const {
+    control,
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const [form] = Form.useForm();
+
+  // console.log(watch("firstName"));
+
+  const onSubmit = (data) => {
+    console.log("Received values of form: ", data);
+  };
+
+  const prefixSelector = (
+    <Form.Item name="prefix" noStyle>
+      <Select className="w-[70px]">
+        <Option value="86">+86</Option>
+        <Option value="87">+87</Option>
+      </Select>
+    </Form.Item>
+  );
+
   return (
-    <div className="px-8 sm:px-12 py-16 shadow-xl rounded-lg border bg-white sm:w-[30rem]">
-      <div className="text-3xl font-bold py-2 text-primary-100">
+    <div className="p-8 sm:px-12 shadow-xl rounded-lg border bg-white sm:w-[30rem]">
+      <div className="text-3xl font-bold pb-8 text-primary-100">
         Sign up to your account
       </div>
-      <div className="text-gray-500 py-2 text-sm">Sign up with</div>
 
-      <div className="grid grid-cols-3 gap-4">
-        <ClientSocialButton SocialIcon={<BsGoogle />} />
-        <ClientSocialButton SocialIcon={<BsTwitter />} />
-        <ClientSocialButton SocialIcon={<BsFacebook />} />
-      </div>
-
-      <div className="grid grid-cols-3 gap-1 items-center py-4">
-        <hr />
-        <div className="text-gray-500 text-xs">Or continue with</div>
-        <hr />
-      </div>
-
-      <div className="pb-4">
-        <label
-          htmlFor="name"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Name
-        </label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          placeholder="Your name"
-          className="mt-1 p-2 block w-full shadow-sm text-sm border
-          border-gray-300 rounded-md text-gray-700"
+      <Form
+        name="register"
+        form={form}
+        layout="vertical"
+        onFinish={handleSubmit(onSubmit)}
+      >
+        <Controller
+          name="firstName"
+          control={control}
+          render={({ field }) => (
+            <Form.Item
+              name="firstName"
+              label="First Name"
+              rules={[
+                { required: true, message: "Please input your First Name!" },
+              ]}
+            >
+              <Input {...field} />
+            </Form.Item>
+          )}
         />
-      </div>
 
-      <div className="pb-2">
-        <label
-          htmlFor="email-address"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Email address
-        </label>
-        <input
-          type="text"
-          name="email-address"
-          id="email-address"
-          autoComplete="email"
-          placeholder="john_doe@gmail.com"
-          className="mt-1 p-2 block w-full shadow-sm text-sm border
-          border-gray-300 rounded-md text-gray-700"
+        <Controller
+          name="lastName"
+          control={control}
+          render={({ field }) => (
+            <Form.Item
+              name="lastName"
+              label="Last Name"
+              rules={[
+                { required: true, message: "Please input your Last Name!" },
+              ]}
+            >
+              <Input {...field} />
+            </Form.Item>
+          )}
         />
-      </div>
 
-      <div className="py-2">
-        <label
-          htmlFor="password"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Password
-        </label>
-        <input
-          type="password"
+        <Controller
+          name="email"
+          control={control}
+          render={({ field }) => (
+            <Form.Item
+              name="email"
+              label="Email"
+              rules={[
+                { required: true, message: "Please input your email!" },
+                { type: "email", message: "Invalid email" },
+              ]}
+            >
+              <Input {...field} />
+            </Form.Item>
+          )}
+        />
+
+        <Controller
+          name="mobile"
+          control={control}
+          render={({ field }) => (
+            <Form.Item
+              name="mobile"
+              label="Mobile"
+              rules={[
+                { required: true, message: "Please input your mobile number!" },
+                /** todo: enable mobile regex based in country */
+                // {
+                //   pattern: /^\+1\d{3}\s?\d{3}\s?\d{4}$/i,
+                //   message: "Invalid mobile number",
+                // },
+              ]}
+            >
+              <Input {...field} addonBefore={prefixSelector} />
+            </Form.Item>
+          )}
+        />
+
+        <Controller
           name="password"
-          id="password"
-          placeholder="**********"
-          className="mt-1 p-2 block w-full shadow-sm text-sm border
-          border-gray-300 rounded-md text-gray-700"
+          control={control}
+          render={({ field }) => (
+            <Form.Item
+              name="password"
+              label="Password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+                {
+                  min: 8,
+                  message: "Password must be at least 8 characters long",
+                },
+              ]}
+              hasFeedback
+            >
+              <Input.Password {...field} />
+            </Form.Item>
+          )}
         />
-      </div>
 
-      <div className="w-full py-2">
-        <button
-          type="submit"
-          className="bg-primary-100 hover:bg-gray-300 hover hover:text-primary-100 text-white font-bold 
-          py-2 px-4 rounded w-full"
-        >
-          Create account
-        </button>
-      </div>
+        <Controller
+          name="confirm"
+          control={control}
+          render={({ field }) => (
+            <Form.Item
+              name="confirm"
+              label="Confirm Password"
+              dependencies={["password"]}
+              hasFeedback
+              rules={[
+                {
+                  required: true,
+                  message: "Please confirm your password!",
+                },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue("password") === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(
+                      new Error(
+                        "The two passwords that you entered do not match!"
+                      )
+                    );
+                  },
+                }),
+              ]}
+            >
+              <Input.Password {...field} />
+            </Form.Item>
+          )}
+        />
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" name="submit">
+            Register
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
