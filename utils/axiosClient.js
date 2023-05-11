03b3e963984aa909df0daaea09977602e6f3175f
nischal <nischaldutt01@gmail.com>
2023-05-11 15:19:38 +0530

    update: configuring request header in axios client instance

--- utils/axiosClient.js
+++ utils/axiosClient.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 const axiosClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_API,
-  // headers: { "X-Custom-Header": "foobar" },
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
 });
 
 export default axiosClient;
