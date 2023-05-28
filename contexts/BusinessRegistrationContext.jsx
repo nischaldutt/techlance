bad25ae98dbe88247c8f56cda5559c8824a1b564
nischal <nischaldutt01@gmail.com>
2023-05-28 22:05:46 +0530

    feat: BusinessRegistrationProvider

---
+++ contexts/BusinessRegistrationContext.jsx
@@ -0,0 +1,101 @@
+import { useReducer, createContext, useContext } from "react";
+
+import { APP_CONSTANTS } from "@/constants";
+import { useBusinessBasicInfo } from "@/hooks";
+
+const BusinessRegistrationContext = createContext(null);
+const BusinessRegistrationDispatchContext = createContext(null);
+
+export function BusinessRegistrationProvider({ children }) {
+  const [businessObj, dispatch] = useReducer(reducer, intialState);
+
+  return (
+    <BusinessRegistrationContext.Provider value={{ businessObj }}>
+      <BusinessRegistrationDispatchContext.Provider value={{ dispatch }}>
+        {children}
+      </BusinessRegistrationDispatchContext.Provider>
+    </BusinessRegistrationContext.Provider>
+  );
+}
+
+export function useBusinessRegistrationContext() {
+  return useContext(BusinessRegistrationContext);
+}
+
+export function useBusinessRegistrationDispatchContext() {
+  return useContext(BusinessRegistrationDispatchContext);
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case APP_CONSTANTS.REDUCER_ACTION_TYPES.BUSINESS_REGISTRATION
+      .ADD_BASIC_INFO: {
+      return action.payload;
+    }
+    case APP_CONSTANTS.REDUCER_ACTION_TYPES.BUSINESS_REGISTRATION
+      .ADD_INSURANCE: {
+      return action.payload;
+    }
+    case APP_CONSTANTS.REDUCER_ACTION_TYPES.BUSINESS_REGISTRATION
+      .ADD_REFERENCE: {
+      return action.payload;
+    }
+    case APP_CONSTANTS.REDUCER_ACTION_TYPES.BUSINESS_REGISTRATION
+      .ADD_SERVICES: {
+      return action.payload;
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+const intialState = {
+  basicInfo: {
+    name: "",
+    address: "",
+    hst: "",
+    unit: null,
+    website: "",
+    discoverDescription: "",
+    industryStandardAgreement: true,
+  },
+  insuranceInfo: {
+    businessId: null,
+    brokerEmail: "",
+    contactBrokerPermission: true,
+    insurancePolicyNumber: "",
+    insuranceAgreement: true,
+  },
+  referencesInfo: [
+    {
+      businessId: null,
+      fullName: "",
+      relationship: "",
+      company: "",
+      email: "",
+      phone: "",
+      description: "",
+    },
+    {
+      businessId: null,
+      fullName: "",
+      relationship: "",
+      company: "",
+      email: "",
+      phone: "",
+      description: "",
+    },
+  ],
+  servicesInfo: {
+    services: [
+      {
+        name: "",
+        description: "",
+        price: 0.0,
+        subCategoryId: null,
+      },
+    ],
+    businessId: null,
+  },
+};
