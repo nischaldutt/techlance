3350bde42e656d85fa2d52f600bebc0965f715eb
nischal <nischaldutt01@gmail.com>
2023-06-01 08:08:50 +0530

    update: business/register

--- hooks/index.js
+++ hooks/index.js
@@ -2,5 +2,5 @@ export { default as useSignup } from "@/hooks/auth/useSignup";
 export { default as useCategories } from "@/hooks/Customer/Categories/useCategories";
 export { default as useSubCategories } from "@/hooks/Customer/SubCategories/useSubCategories";
 export { default as useSubCategoriesWithCategoryId } from "@/hooks/Customer/SubCategories/useSubCategoriesWithCategoryId";
-export { default as useCreateBusinessBasicInfo } from "@/hooks/Business/Registration/useCreateBusinessBasicInfo";
-export { default as useCreateBusinessInsurance } from "@/hooks/Business/Registration/useCreateBusinessInsurance";
+export { default as useCreateBusinessBasicInfo } from "@/hooks/business/register/useCreateBusinessBasicInfo";
+export { default as useCreateBusinessInsurance } from "@/hooks/business/register/useCreateBusinessInsurance";
