0e1c515bb385e6b76c6656cac6364504bb296c9f
nischal <nischaldutt01@gmail.com>
2023-09-24 12:10:53 +0530

    update: removing react-hook-form controller

--- components/business/RegistrationSteps/InsuranceInfo.jsx
+++ components/business/RegistrationSteps/InsuranceInfo.jsx
@@ -1,4 +1,3 @@
-import { useForm as useReactHookForm, Controller } from "react-hook-form";
 import { Button, Form, Input, Checkbox } from "antd";
 
 import { useAntdMessageContext, useQueryCacheContext } from "@/contexts";
@@ -8,13 +7,12 @@ import { APP_CONSTANTS } from "@/constants";
 
 const InsuranceInfo = ({ previous, next }) => {
   const [form] = Form.useForm();
-  const { control, handleSubmit } = useReactHookForm();
   const { successMessage, errorMessage } = useAntdMessageContext();
   const { getQueryFromCache } = useQueryCacheContext();
 
-  const cachedInsuranceInfo = getQueryFromCache([
-    APP_CONSTANTS.QUERY_KEYS.BUSINESS.BUSINESS_REGISTRATION.ADD_INSURANCE,
-  ]);
+  const cachedInsuranceInfo = getQueryFromCache(
+    APP_CONSTANTS.QUERY_KEYS.BUSINESS.BUSINESS_REGISTRATION.ADD_INSURANCE
+  );
 
   const { createBusinessInsurance, isLoading } = useCreateBusinessInsurance(
     (isSuccess, response) => {
@@ -40,117 +38,90 @@ const InsuranceInfo = ({ previous, next }) => {
         name="insuranceInfo"
         form={form}
         layout="vertical"
-        onFinish={handleSubmit(onSubmit)}
+        onFinish={onSubmit}
         autoComplete="off"
         initialValues={cachedInsuranceInfo}
         requiredMark="optional"
       >
-        <Controller
+        <Form.Item
+          label="Email of your Insurance Agent/Broker"
           name="brokerEmail"
-          control={control}
-          render={({ field }) => (
-            <Form.Item
-              label="Email of your Insurance Agent/Broker"
-              name="brokerEmail"
-              rules={[
-                { required: true, message: "Please enter agent's email!" },
-                { type: "email", message: "Invalid email" },
-              ]}
-              className="my-0"
-            >
-              <Input placeholder="john.doe@gmail.com" {...field} />
-            </Form.Item>
-          )}
-        />
+          rules={[
+            { required: true, message: "Please enter agent's email!" },
+            { type: "email", message: "Invalid email" },
+          ]}
+          className="my-0"
+        >
+          <Input placeholder="john.doe@gmail.com" />
+        </Form.Item>
 
-        <Controller
+        <Form.Item
+          label=""
           name="contactBrokerPermission"
-          control={control}
-          render={({ field }) => (
-            <Form.Item
-              label=""
-              name="contactBrokerPermission"
-              valuePropName="checked"
-              className="my-0"
-              rules={[
-                { required: true, message: "Permission Required!" },
-                { type: "boolean", message: "Invalid value!" },
-              ]}
-            >
-              <Checkbox {...field}>
-                Allow Techlance to contact my Agent/Broker
-              </Checkbox>
-            </Form.Item>
-          )}
-        />
+          valuePropName="checked"
+          className="my-0"
+          rules={[
+            { required: true, message: "Permission Required!" },
+            { type: "boolean", message: "Invalid value!" },
+          ]}
+        >
+          <Checkbox>Allow Techlance to contact my Agent/Broker</Checkbox>
+        </Form.Item>
 
-        <Controller
+        <Form.Item
+          label="Insurance Policy Number"
           name="insurancePolicyNumber"
-          control={control}
-          render={({ field }) => (
-            <Form.Item
-              label="Insurance Policy Number"
-              name="insurancePolicyNumber"
-              rules={[
-                {
-                  required: true,
-                  message: "Insurance Policy number required!",
-                },
-                {
-                  pattern:
-                    APP_CONSTANTS.REGEXES.BUSINESS_INSURANCE_POLICY_NUMBER,
-                  message: "Invalid Insurance Policy number!",
-                },
-              ]}
-            >
-              <Input placeholder="eg. 12345" {...field} />
-            </Form.Item>
-          )}
-        />
+          rules={[
+            {
+              required: true,
+              message: "Insurance Policy number required!",
+            },
+            {
+              pattern: APP_CONSTANTS.REGEXES.BUSINESS_INSURANCE_POLICY_NUMBER,
+              message: "Invalid Insurance Policy number!",
+            },
+          ]}
+        >
+          <Input placeholder="eg. 12345" />
+        </Form.Item>
 
-        <Controller
+        <Form.Item
+          label={
+            <div className="flex flex-col gap-4 p-3 bg-gray-100 rounded-lg">
+              <p className="font-bold">I certify that: </p>
+              <p className="text-justify">
+                I work solely by myself, with no full-time or part-time
+                employees, contractors or sub-trades.
+              </p>
+              <p className="font-bold text-center">OR</p>
+              <p className="text-justify">
+                I have full-time and/or part-time employees or employ
+                contractors or sub-trades as needed. I have an active WSIB
+                number.
+              </p>
+            </div>
+          }
           name="insuranceAgreement"
-          control={control}
-          render={({ field }) => (
-            <Form.Item
-              label={
-                <div className="flex flex-col gap-4 p-3 bg-gray-100 rounded-lg">
-                  <p className="font-bold">I certify that: </p>
-                  <p className="text-justify">
-                    I work solely by myself, with no full-time or part-time
-                    employees, contractors or sub-trades.
-                  </p>
-                  <p className="font-bold text-center">OR</p>
-                  <p className="text-justify">
-                    I have full-time and/or part-time employees or employ
-                    contractors or sub-trades as needed. I have an active WSIB
-                    number.
-                  </p>
-                </div>
-              }
-              name="insuranceAgreement"
-              valuePropName="checked"
-              rules={[
-                { required: true, message: "" },
-                { type: "boolean", message: "Invalid value!" },
-                ({ getFieldValue }) => ({
-                  validator(_, value) {
-                    if (value) {
-                      return Promise.resolve();
-                    }
-                    return Promise.reject(
-                      new Error("Please accept the Insurance agreement!")
-                    );
-                  },
-                }),
-              ]}
-            >
-              <Checkbox {...field}>
-                I agree and acknowledge that one of the statements above is true
-              </Checkbox>
-            </Form.Item>
-          )}
-        />
+          valuePropName="checked"
+          rules={[
+            { required: true, message: "" },
+            { type: "boolean", message: "Invalid value!" },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  new Error("Please accept the Insurance agreement!")
+                );
+              },
+            }),
+          ]}
+        >
+          <Checkbox>
+            I agree and acknowledge that one of the statements above is true
+          </Checkbox>
+        </Form.Item>
 
         <div className="flex justify-between">
           <Form.Item>
