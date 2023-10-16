4c6ec4188f48d51e6d1dcfaacc0e9c17a71eef17
nischal <nischaldutt01@gmail.com>
2023-10-16 12:52:15 +0530

    update: sign up response

--- hooks/auth/useSignup.js
+++ hooks/auth/useSignup.js
@@ -1,39 +1,20 @@
 import { useMutation } from "@tanstack/react-query";
 
-import axiosClient from "@/libs/axiosClient";
-import { APP_CONSTANTS, URL_CONSTANTS } from "@/constants";
+import { postRequest } from "@/services";
+import { URL_CONSTANTS } from "@/constants";
 
 export default function useSignup(callback) {
-  const { mutate, isLoading } = useMutation({
-    mutationFn: (userObj) => {
-      const { user_type, firstName, lastName, email, mobile, password } =
-        userObj;
-      const reqBody = {
-        user_type: user_type,
-        first_name: firstName,
-        last_name: lastName,
-        email: email,
-        mobile_no: parseInt(mobile),
-        password: password,
-        country_id: 1,
-        is_mobile_verified: 1,
-        is_email_verified: 1,
-      };
-
-      const signupEndpoint =
-        userObj?.user_type === APP_CONSTANTS.USER_TYPE.CUSTOMER
-          ? URL_CONSTANTS.CUSTOMER.AUTH.SIGN_UP
-          : URL_CONSTANTS.BUSINESS.AUTH.SIGN_UP;
-
-      return axiosClient.post(signupEndpoint, reqBody);
+  const { mutate: signup, isLoading } = useMutation({
+    mutationFn: (reqBody) => {
+      return postRequest(URL_CONSTANTS.AUTH.SIGN_UP, reqBody);
     },
-    onSuccess: (data) => {
-      callback(true, data?.message);
+    onSuccess: (response) => {
+      callback(true, response?.message);
     },
     onError: (error) => {
-      callback(false, error?.response?.data?.message);
+      callback(false, error?.payload?.message);
     },
   });
 
-  return { mutate, isLoading };
+  return { signup, isLoading };
 }
